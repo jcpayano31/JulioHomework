ffe7544dc5723443186fcade555d41f17bd5b794
jcpayano31 <jcpayano@hotmail.com>
2019-06-03 22:28:31 -0400

    Birth card Generator

---
+++ HomeWork6/BirthdayCard/BirthdayCard/Models/BirthDayCardSend.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BirthdayCard.Models
+{
+    public class BirthDayCardSend
+    {
+        [Required(ErrorMessage ="From User Required")]
+        public string FromUser { get; set; }
+        [Required(ErrorMessage ="To User Required")]
+        public string ToUser { get; set; }
+        [Required(ErrorMessage ="Menssage Required")]
+        public string BCardMenssege { get; set; }
+    }
+}
